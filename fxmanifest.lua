488a4cbac2c5e4b9226b6423f05a88eb98a7f090
chickenlord01 <58153078+chickenlord01@users.noreply.github.com>
2023-12-10 12:13:56 -0600

    update version to 1.2.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ lua54 'yes'
 
 name 'johnspolicerework'
 description 'Police Menu for Menu servers'
-version '1.2.0'
+version '1.2.1'
 url 'https://github.com/chickenlord01/johnspolicemenu'
 author 'JohnL#6869'
 
