2e5043b7be68995a05198a25aea677b55749f6b6
chickenlord01 <58153078+chickenlord01@users.noreply.github.com>
2024-02-13 17:54:30 -0600

    add menu state saving

--- server/server.lua
+++ server/server.lua
@@ -102,3 +102,64 @@ AddEventHandler('onResourceStart', function(resource)
     end
 end)
 --End *Update check*
+
+local playerdata = {}
+function LoadData()
+    playerdata = json.decode(LoadResourceFile(GetCurrentResourceName(), "playerdata.json"))
+end
+function SaveData()
+    --playerdata = {["license:86ea33798ffbb7186f3bee040be8fe0922561bb2"]={position="top-right",type="none",data={}}}
+    SaveResourceFile(GetCurrentResourceName(), "playerdata.json",json.encode(playerdata,{indent=true}),-1)
+end
+AddEventHandler('onResourceStart', function(resource)
+    if resource == GetCurrentResourceName() then
+        LoadData()
+    end
+end)
+AddEventHandler('onResourceStop', function(resource)
+    if resource == GetCurrentResourceName() then
+        SaveData()
+    end
+end)
+
+function GetPlayerData(source)
+    local identifiers = GetPlayerIdentifiers(source)
+    for _, v in pairs(identifiers) do
+        if string.find(v, "license:") then
+            return playerdata[v]
+        end
+    end
+end
+lib.callback.register('getPlayerData', GetPlayerData)
+
+function UpdatePlayerData(source,data)
+    local identifiers = GetPlayerIdentifiers(source)
+    for _, v in pairs(identifiers) do
+        if string.find(v, "license:") then
+            playerdata[v] = data
+            if playerdata[v].private then
+                playerdata[v].private = nil
+            end
+            return 1
+        end
+    end
+end
+lib.callback.register('updatePlayerData', UpdatePlayerData)
+
+AddEventHandler("playerConnecting", function(name, setKickReason, deferrals)
+    local player = source
+    local identifiers = GetPlayerIdentifiers(player)
+
+    Wait(0)
+    for _, v in pairs(identifiers) do
+        if string.find(v, "license:") then
+            if not playerdata[v] then
+                playerdata[v] = {
+                    type = Config.ForceType,
+                    position = 'top-right',
+                    data = {}
+                }
+            end
+        end
+    end
+end)
